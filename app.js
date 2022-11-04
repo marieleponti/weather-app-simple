7e62ef01c1d6aa0621c344b29944942f9f95b022
Mariele Ponticiello <mponticiello@beltrailway.com>
2022-11-04 10:08:56 -0500

    commit project

---
+++ app.js
@@ -0,0 +1,39 @@
+const { response } = require("express");
+const express = require ("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+    const apiKey = "24c2802682a6eb405eb7b276801f32bc";
+    var query = req.body.zip;
+    var units = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=" + units + "&appid=" + apiKey;
+
+    https.get(url, function(response){
+        console.log(response.statusCode);
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const desc = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const iconUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            res.write("<p>The weather in the area of the zip " + query + " is currently " + desc + ".");
+            res.write("<p>The temp is currently " + temp + " degrees Celsius.");
+            res.write("<img src=" + iconUrl + ">");
+            res.send();
+        });
+    });
+
+});
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000.")
+})
